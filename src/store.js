5c7a11b4fb7024302f60ea2b5c3358e250e60129
sazzadur rahman <sazzadurrahman88@gmail.com>
2021-04-12 10:14:27 +0600

    plugins added to save current board data to localstorage

--- src/store.js
+++ src/store.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import DefaultDashboard from "./base-board";
+import DefaultBoard from "./base-board";
+import { saveStatePlugin } from "./utils";
 Vue.use(Vuex);
 
+const board = JSON.parse(localStorage.getItem("board")) || DefaultBoard;
+
 const store = new Vuex.Store({
+  plugins: [saveStatePlugin],
   state: {
-    board: DefaultDashboard,
+    board,
   },
   mutations: {},
 });
